2fa726761f78e7bbf42bb2e2bfbf09153970c729
Dustin Riley <driley137@fullsail.edu>
2014-03-26 21:32:04 -0400

    First Function.
    About to test decimalPlaceFormat

--- riley_dustin_project4.js
+++ riley_dustin_project4.js
@@ -6,13 +6,29 @@
  * Project 4
 */
 
-// Global Variables; Assign variables for the function calls.
+// Global Variables; Assign variables for the function calls. --------------------------------------------------
+var numToBeFormatted = "3.12890", decimals = "3"   // Decimal Format variables
+
+
+// Functions ----------------------------------------------------------------------------------------------------
+
+// Function to evaluate a string to find out if it follows a phone number format. Boolean return.
+var strPhoneNumberCheck = function(phNumber) {
+    
+    
+}; // END   s t r P h o n e N u m b e r C h e c k ()
+
+
+// Function to format a number to requested decimal places.
+var decimalPlaceFormat = function (number, decimalPlaces){
+    // Parse the string number to a float, then use the toFixed() for the decimal.
+    // Reason variable is string is we can handle formatting all at once in this function.
+    number = parseFloat(number.toFixed(decimalPlaces));
+    return number;   
+}; // END   d e c i m a l P l a c e F o r m a t ()
 
 
 
-// Functions
 
-
-
-
-// Main code; Where the functions will be called from.
+// Main code; Where the functions will be called from. ----------------------------------------------------------
+console.log(decimalPlaceFormat(numToBeFormatted, decimals));  // Decimal Format
