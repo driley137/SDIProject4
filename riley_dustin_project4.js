8a51267b497b9f10b9e69beb5315780feecde79a
Dustin Riley <driley137@fullsail.edu>
2014-03-26 23:42:45 -0400

    3 down. 3 to go.
    I spent a little more time on the “Interactive” portion of the array as
    I re-taught myself on how to prompt a number of times to make a custom
    array.
    
    So far, this is the funnest assignment we’ve had in SDI. I love
    especially am loving getting creative with the “base code” portion to
    make it so you can interact with it and use your own variable data if
    you’d like. I know that’s not graded, but I’m confident that I’ll have
    the other functions done with plenty of time.

--- riley_dustin_project4.js
+++ riley_dustin_project4.js
@@ -9,6 +9,7 @@
 // Global Variables; Assign variables for the function calls. --------------------------------------------------
 var numToBeFormatted = "3.12890", decimalPlaces = "2"   // Decimal Format variables
 var number1 = 5, number2 = 10, fuzzyPercent = "50"      // Fuzzy Match variables
+var arrNumbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20], stoppingNumber = 13 // Next Smallest Array variables
 
 // Functions ----------------------------------------------------------------------------------------------------
 
@@ -60,6 +61,29 @@ var fuzzyMatch = function(num1, num2, percent) {
     
 } // END    f u z z y M a t c h ()
 
+// Function to return the next numerical to the comparable input number, from an array.
+var arrNextSmallest = function(arr, number) {
+    var winningNumber = 0;
+    number = parseInt(number);  // QA
+    
+    for(var i = 0; arr[i] != arr.length; i++){
+        // This will loop through the entire array. I'm sure I could use another loop in order to exit once found.
+        // But I think this way, even if the numbers aren't in nummerical order it'll work without me rearranging the array.
+        var testNumber = arr[i]; // Local variable assigning via the value of the index
+
+        if (testNumber > number && (testNumber - winningNumber) > (testNumber - number)) {
+            // I believe this will pull the closest numberical value
+            winningNumber = testNumber;
+            
+        } // end IF statement. Else condition is below
+        // Not doing anything in the "else" area of the IF statement. I only want something to happen IF it's true.
+    } // end for loop to assign next highest to return variable
+
+    return winningNumber;
+    
+} // END   a r r N e x t S m a l l e s t ()
+
+
 
 // Main code; Where the functions will be called from. ----------------------------------------------------------
 
@@ -73,12 +97,25 @@ decimalPlaces = prompt("How many decimal places do you want it formatted to?");
 console.log("Your number formatted into the decimals you requested is " + decimalPlaceFormat(numToBeFormatted, decimalPlaces));  // Decimal Format
 
 
-/*  REMOVE ME TO INTERACT WITH DECIMAL FORMATTING FUNCTION  !!!! START
+/*  REMOVE ME TO INTERACT WITH FUZZY MATCH FUNCTION  !!!! START
 
 number1 = prompt("What is your first number?");
 number2 = prompt("What is your second number?");
 fuzzyPercent = prompt("What percentage are you looking for? Please no symbols.");
 
-*/  // REMOVE ME TO INTERACT WITH DECIMAL FORMATTING FUNCTION !!!! END
-console.log(fuzzyMatch(number1, number2, fuzzyPercent));
+*/  // REMOVE ME TO INTERACT WITH FUZZY MATCH FUNCTION !!!! END
+console.log(fuzzyMatch(number1, number2, fuzzyPercent));                                                        // Fuzzy Match
+
+
+/*  REMOVE ME TO INTERACT WITH NEXT SMALLEST ARRAY FUNCTION  !!!! START
+
+arrNumbers = Array(Number(prompt("How many entries in your custom array, do you want?")));
+for (var i = 0; i < arrNumbers.length; i++){
+  arrNumbers[i] = prompt("Enter your number for position " + (i+1) + ".");  
+} // end for loop
+stoppingNumber = prompt("What number do you want to evaluate with?");
+
+*/  // REMOVE ME TO INTERACT WITH NEXT SMALLEST ARRAY FUNCTION !!!! END
+console.log("The next smallest number within the array is " + arrNextSmallest(arrNumbers, stoppingNumber) + "!");      // Next Smallest Array
+
 
